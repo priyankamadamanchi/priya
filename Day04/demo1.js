8bae944deb8404f7a7d71aaaaaf5a70a531ef69f
priyanka <sripriyanka0624@gmail.com>
2025-07-06 12:50:53 -0400

    changed made

---
+++ Day04/demo1.js
@@ -0,0 +1,6 @@
+var a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+var x = 10;
+console.log(x, typeof x);
---
+++ Day04/demo1.ts
@@ -0,0 +1,10 @@
+let a =10;
+
+console.log(a);
+
+a=20;
+console.log(a);
+
+let x: number = 10;
+console.log(x , typeof x);
+
